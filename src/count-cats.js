95ef8998f9fbfac6dd29df1aea350afef16ea581
Oxana Savianok <Oxana8833@gmail.com>
2022-03-10 22:30:55 +0300

    feat: add decision countcats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  //throw new NotImplementedError('Not implemented');
+  let count = 0;
+  matrix.forEach ((arr) => {
+    arr.forEach ((elem) => {
+      if (elem === '^^') {
+        count += 1;
+      }
+    })
+  })
+  return count;
 }
 
 module.exports = {
