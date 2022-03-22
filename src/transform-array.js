2ace3ed5f6fdc1e392b07976767ef589559e7e3a
Oxana Savianok <Oxana8833@gmail.com>
2022-03-22 22:22:11 +0300

    feat: add decision hanoi-tower

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  let from = 0;
+  if(!Array.isArray(arr)) {
+    return `'arr' parameter must be an instance of the Array!`}
+    arr.forEach((elem) => {
+      let k = arr.indexOf('--disard-next', from);
+      if (k >= 0) {
+        arr.splice(k, 2)
+        from = k;
+      }
+      from = 0;
+       
+      let l = arr.indexOf('--discard-prev', from);
+      if (l >= 0) {
+        arr.splice(l - 1, 2)
+        from = l;
+      }
+      from = 0;
+
+      let n = arr.indexOf('--double-next', from);
+      if (n >= 0) {
+        arr.splice(n, 1, arr[n+1])
+        from = n;
+      }
+      from = 0;
+      
+      let m = arr.indexOf('--double-prev', from);
+      if (m >= 0) {
+        arr.splice(m, 1, arr[m-1])
+        from = m;
+      }
+      from = 0;
+    })
+    return arr;
+  }
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-}
+
 
 module.exports = {
   transform
