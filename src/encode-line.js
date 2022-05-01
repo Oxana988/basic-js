903ad82719d08b98e4e7ee54237e6154cffabdf6
Oxana Savianok <Oxana8833@gmail.com>
2022-05-01 21:53:50 +0300

    feat: add decision for encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,10 +10,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ function encodeLine(str) {
+  let result = '';
+   let sum = 1;
+   for (let i = 0; i < str.length; i++) {
+     if (str[i] !== str[i + 1]) {
+       result += sum + str[i];
+       sum = 1;
+     } else {
+       sum += 1;
+       str = str.slice(0, (i + 1)) + str.slice(i + 1);
+     }
+     
+   }
+   return result.replace(/1/g, '');
+ }
 
 module.exports = {
   encodeLine
