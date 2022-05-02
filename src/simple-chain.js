5e8c768ca93d99dfe8071b21acc7b327759213e8
Oxana Savianok <Oxana8833@gmail.com>
2022-05-02 14:35:29 +0300

    feat: add decision for deptharr

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,30 +4,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-/*const chainMaker = {
-  currentChain: [],
- 
-  
-  removeLink(position) {
-    if (!Number.isInteger(position) || this.currentChain[position - 1] === undefined) {
-    //if (position < 1 || position > this.getLength() || isNaN(position) || this.currentChain[position - 1] === undefined) {
-      this.currentChain.length = 0;
-      throw new Error("You can't remove incorrect link!");
-    }
 
-    this.currentChain.splice(position - 1, 1);
-    return this;
-  },
-  reverseChain() {
-    this.currentChain.reverse();
-    return this;
-  },
-  finishChain() {
-    let str = this.currentChain.join('~~');
-    this.chain = [];
-    return str;
-  }
-};*/
  const chainMaker = {
   currentChain: [],
 
