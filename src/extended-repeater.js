04ba7dee457318dda5b098b8a6bac490fdde31f3
Oxana Savianok <Oxana8833@gmail.com>
2022-05-02 15:38:47 +0300

    feat: add decision for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+
+  options.separator = options.separator || '+';
+  options.additionSeparator = options.additionSeparator || '|';
+  options.additionRepeatTimes = options.additionRepeatTimes || 1;
+  options.repeatTimes = options.repeatTimes || 1;
+  options.addition =
+  options.addition !== undefined ? String(options.addition) : '';
+      /*if (typeof str !== 'string') str = String(str);*/
+  const newStr = new Array(options.additionRepeatTimes).fill(options.addition).join(options.additionSeparator);
+
+  return new Array(options.repeatTimes).fill(str + newStr).join(options.separator);
+ 
 }
 
 module.exports = {
