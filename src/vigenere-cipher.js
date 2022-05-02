c99889a31004a0eed5013bd9d8738a79c4ad49b9
Oxana Savianok <Oxana8833@gmail.com>
2022-05-02 23:15:07 +0300

    feat: add decision for cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(type = true) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+
+ encrypt(msg, key) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (!(msg && key)) {
+      throw new Error(`Incorrect arguments!`);
+    }
+
+    let codeStr = [];
+    msg = msg.toUpperCase();
+    key = key.toUpperCase();
+
+    let cipher = key.repeat(Math.ceil(msg.length / key.length)).split('');
+    for (let i = 0; i < msg.length; i++) {
+      if (msg.charCodeAt(i) >= 65 && msg.charCodeAt(i) <= 91) {
+        codeStr.push(String.fromCharCode(65 + ((msg.charCodeAt(i) + cipher[0].charCodeAt(0)) % 26)));
+        cipher.shift();
+      } else {
+        codeStr.push(msg.charAt(i));
+      }
+    }
+
+    if (this.type === false) return codeStr.reverse().join('');
+    return codeStr.join('');
+  }
+
+  
+  decrypt(encMsg, key) {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if (!(encMsg && key)) {
+      throw new Error(`Incorrect arguments!`);
+    }
+
+    let invStr = [];
+    encMsg = encMsg.toUpperCase();
+    key = key.toUpperCase();
+
+    let cipher = key.repeat(Math.ceil(encMsg.length / key.length)).split('');
+    for (let i = 0; i < encMsg.length; i++) {
+      if (encMsg.charCodeAt(i) >= 65 && encMsg.charCodeAt(i) <= 91) {
+        invStr.push(String.fromCharCode(65 + ((encMsg.charCodeAt(i) + 26 - cipher[0].charCodeAt(0)) % 26)));
+        cipher.shift();
+      } else {
+        invStr.push(encMsg.charAt(i));
+      }
+    }
+
+    if (this.type === false) return invStr.reverse().join('');
+    return invStr.join('');
+  
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
